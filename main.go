cb8b3de5f54849c64923b9c934fc44a0838abd2e
JonasUJ <jonas@ulsnes.dk>
2022-10-25 19:28:23 +0200

    Remove logfile

--- main.go
+++ main.go
@@ -2,15 +2,15 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"log"
 	"os"
 )
 
 var (
-	start   = flag.String("start", "client", "Either client or server")
-	name    = flag.String("name", "<no name>", "Name of this participant")
+	start   = flag.String("start", "client", "Entrypoint for the application. Either client or server")
+	name    = flag.String("name", "NoName", "Name of this instance")
 	port    = flag.String("port", "50050", "Port to connect to")
-	logfile = flag.String("logfile", "", "Log file")
 )
 
 func main() {
@@ -19,18 +19,19 @@ func main() {
 	log.SetOutput(os.Stderr)
 	log.SetFlags(log.Ltime)
 
-	if *logfile != "" {
-		file, err := os.OpenFile(*logfile, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666)
-		if err != nil {
-			log.Fatalf("could not open file %s: %v", *logfile, err)
-		}
-		log.SetOutput(file)
+	logfile := fmt.Sprintf("%s.log", *name)
+	file, err := os.OpenFile(logfile, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666)
+	if err != nil {
+		log.Fatalf("could not open file %s: %v", logfile, err)
 	}
+	log.SetOutput(file)
 
 	// Can't have two main functions in the same package
 	if *start == "server" {
 		server()
-	} else {
+	} else if *start == "client" {
 		client()
+	} else {
+		log.Fatalf("start not a valid value '%s' - expected on of 'client' or 'server'", *start)
 	}
 }
