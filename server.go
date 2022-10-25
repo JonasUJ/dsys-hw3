272933409bada9000a1393d66b81990afedc6a03
JonasUJ <jonas@ulsnes.dk>
2022-10-25 20:19:51 +0200

    Lamport for Server

--- server.go
+++ server.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"fmt"
 	"io"
 	"net"
+	"os"
 
 	"github.com/JonasUJ/dsys-hw3/chittychat"
+	"github.com/JonasUJ/dsys-hw3/lamport"
 	"golang.org/x/exp/slices"
 	"google.golang.org/grpc"
 )
@@ -13,18 +16,35 @@ type Server struct {
 	chittychat.UnimplementedChatServer
 	clients []*chittychat.Chat_ConnectServer
 	chMsgs  chan *chittychat.Message
+	time    uint64
+	pid     uint32
+}
+
+func (server *Server) GetTime() uint64 {
+	return server.time
+}
+
+func (server *Server) GetPid() uint32 {
+	return server.pid
 }
 
 func (s *Server) Connect(stream chittychat.Chat_ConnectServer) error {
 	s.clients = append(s.clients, &stream)
 
+	l.Println("new client connection")
+	// Send message to client to let them sync time
+	stream.Send(lamport.MakeMessage(s, fmt.Sprintf("Welcome to the %s server", *name)))
+
 	for {
 		msg, err := stream.Recv()
 		if err != nil {
+			// Client stream closed: Forget stream.
 			index := slices.Index(s.clients, &stream)
 			s.clients = slices.Delete(s.clients, index, index+1)
 
 			if err == io.EOF {
+				// Connection closed gracefully
+				s.chMsgs <- lamport.MakeMessage(s, "A client left the server")
 				return nil
 			} else {
 				l.Printf("server recv err: %v\n", err)
@@ -32,7 +52,8 @@ func (s *Server) Connect(stream chittychat.Chat_ConnectServer) error {
 			}
 		}
 
-		l.Printf("got msg '%s'\n", msg.Content)
+		l.Printf("recv '%s'\n", msg)
+		s.time = lamport.LamportRecv(s, msg)
 
 		s.chMsgs <- msg
 	}
@@ -49,6 +70,7 @@ func server() {
 	server := &Server{
 		clients: make([]*chittychat.Chat_ConnectServer, 0),
 		chMsgs:  make(chan *chittychat.Message),
+		pid: uint32(os.Getpid()),
 	}
 
 	go func() {
@@ -62,8 +84,9 @@ func server() {
 
 	// Recv messages and send them to everyone
 	for msg := range server.chMsgs {
+		l.Printf("send '%s'", msg)
+		server.time = lamport.LamportSend(server)
 		for _, client := range server.clients {
-			l.Printf("sending '%s'", msg)
 			(*client).Send(msg)
 		}
 	}
